5c51848ae7daede05d440ad1d4a7460edba08baa
sharif <srka780@gmail.com>
2025-12-27 13:51:14 +0600

    TODO: auth system

--- components/auth_components/socialMediaBtns.tsx
+++ components/auth_components/socialMediaBtns.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -5,12 +6,23 @@ import facebook from "@/public/facebookLogin.png";
 import apple from "@/public/appleLogin.png";
 import google from "@/public/googleLogin.png";
 
+import Link from "next/link";
+
 const SocialMediaBtns = () => {
-  const wipClass = "flex flex-col md:flex-row justify-center items-center gap-4";
-  const btnClass = " w-full md:w-28 bg-gray-100 hover:bg-gray-100 shadow-4xl active:shadow-inner border shadow-gray-400 border-gray-200 cursor-pointer"
+  const wipClass =
+    "flex flex-col md:flex-row justify-center items-center gap-4";
+  const btnClass =
+    " w-full md:w-28 bg-gray-100 hover:bg-gray-100 shadow-4xl active:shadow-inner border shadow-gray-400 border-gray-200 cursor-pointer";
+
+  // const loginSocialMedia = async () => {
+  //   console.log("loginSocialMedia");
+  //   const result = await signIn("google", { callbackUrl: "/" });
+  //   if (result?.ok) router.push("/"); dev-h6pe2hhoj2lwc1ek
+  // };
 
   return (
     <div className={wipClass}>
+      <Link href={"/auth/login?connection=google"}>Login</Link>
       <Button className={`${btnClass} `}>
         <Image className=" size-7 " src={facebook} alt=""></Image>
       </Button>
